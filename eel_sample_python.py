50590de66a59a9a3cbd6068400361c388571dece
1234-369-4989 <surasei52000@icloud.com>
2023-11-13 22:10:36 +0900

    app complete

--- eel_sample_python.py
+++ eel_sample_python.py
@@ -7,7 +7,7 @@ import sys
 
 ###
 import json
-import re
+import re # 削除する
 import datetime
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
@@ -18,12 +18,6 @@ import eel
 # Use latest version of Eel from parent directory
 sys.path.insert(1, '../../')
 print(sys.path)
-
-@eel.expose  # Expose function to JavaScript
-def say_hello_py(x):
-    """Print message from JavaScript on app initialization, then call a JS function."""
-    print('Hello from %s' % x)  # noqa T001
-    eel.say_hello_js('Python {from within say_hello_py()}!')
 
 
 @eel.expose
@@ -44,77 +38,45 @@ def pick_file(folder):
     else:
         return '{} is not a valid folder'.format(folder)
 
-
-
-@eel.expose
-def np_softmax(x):
-    f_x = np.exp(x) / np.sum(np.exp(x))
-    return f_x
-
 @eel.expose
 def get_datetime():
     now = datetime.datetime.now()
     dt = now.strftime('%Y%m%d_%H%M%S')
     return dt
 
-@eel.expose
-def split_sentences(text):
-    text = re.sub(r"\n", "", text)  # 改行を除外
 
-    # sentences = re.split(r"。", text)  # 句点で分割
-    sentences = re.split(r"(?<=」)", text)  # 鍵括弧で分割
 
-    # 文章に「。」を後付けで追加
-    for i in range(len(sentences)-1):
-        sentences[i] += "。"
-
-    # 空の文を除外して、各文にIDを付けてリストに格納
-    sentence_list = []
-    for i, sentence in enumerate(sentences):
-        if sentence:
-            sentence_list.append((i+1, sentence.strip()))  # IDと文をタプルとしてリストに追加
-
-    return sentence_list
-
-sentence_list = '「海賊王に俺はなる！」「降りて来いよド三流　格の違いってやつを見せてやる！！」「俺は俺の責務を全うする！！ここにいる者は誰も死なせない！！」「母上　俺はちゃんとやれただろうか　やるべきこと　果たすべきことを　全うできましたか？」「おれは人間をやめるぞ！ジョジョーッ！！」「おれは人間を超越するッ！」「そうくるか！！　女誑しめ！！」「ならばこちらは大義だ」「どけ！！！　俺はお兄ちゃんだぞ！！！」「全力でお兄ちゃんを遂行する！！」「とりあえず1回呼んでみてくれないか？お兄ちゃんと」「そうだよ。エヴァに乗って、世界を変えるんだ！」「舐めるなよ小童が！！！」「そのニヤケ面ごと飲み込んでくれるわ！！！」「真に純粋な本物の"人間"なのだ」「どこをどう見て言っている！！」「失われた命は回帰しない　二度と戻らない」「ヒューズは死んだ！！　もう居ない！！　貴様の行為は火に油を注ぐだけだ！！」「仲間に手を掛けるとはどうゆう了見ダ？」「忘れる訳ないだろうガ！！！」「遅めの反抗期だよ親父殿！！」「バッカじゃないの　綺麗事並べてさ　人情ごっこかい？　虫唾が走る！！」「何だと？　お前　お前はもう　黙れ　煉獄さんのことを喋るな」「逃げるな卑怯者！！」「人の心の中に土足で踏み入るな　俺はお前を許さない」「そんなことが通用するならお前の家族は殺されていない」「焼き殺すぞ」「生殺与奪の権を他人に握らせるな！！」「いいか…てめーらが宇宙のどこで何しよーとかまわねー　だが俺のこの剣　こいつが届く範囲は俺の国だ」「もう喋らなくていいぞエンヴィー　まずその舌の根から焼き尽くしてやろう」「獪岳　鬼になったお前を　俺はもう兄弟子と思わない」「貴様アアア！！逃げるなアア！！！　責任から逃げるなアア」「つらいも何もあるものか　私の姉を殺したのはお前だな？　この羽織に見覚えはないか」「よもやその無理難題を押しつけるために手を組んだのではあるまいな？」「糞が…　ニンゲンふぜいが見下してんじゃねぇ！！」「蒙昧な人間共その一生を幾千積み重ねても釣り合わんぞ」「無惨　お前は　存在してはいけない生き物だ」「惨めったらしくうずくまるのはやめろ！！」「地獄に堕ちろ」「偽物は消えて然るべき」「仲間ってのは魂で繋がってんだヨ！！　魂に染みついちまっているものをすすいで落とす事なんか出来ないんだヨ！！」「言うはずが無いだろうそんなことを　俺の家族が！！」「正気とは思えませんね　貴方頭大丈夫ですか？　本当に吐き気がする」「失礼だな　純愛だよ」「オマエが一番信用できる　そんだけだよ」「本当にまったくその通り!　激情にまかせて吠えたところで得な事なんてありゃしねぇ」「今の私には止めてくれる者や正しい道を示してくれる者がいます」「逆だよ　ゴン　オレなんだ　ゴン　オレ　お前にあえて　本当によかった」「感謝するぜ　お前と出会えた　これまでの全てに！！！」「私はこの人を信用しているし信頼している　でも尊敬はしてません」「友達が友達助けるのは当然だろ」「ああ諸君今日は思ったより早く帰れそうだ　鋼の錬金術師が乗っている」「了解しました　お望みとあらば地獄まで」「あいつが戦って負けるわけがねェ　汚ねェ罠にかけられたに決まってる！！」「友達になるのにだって資格なんていらない！！」「手を貸した方がいいかね？　鋼の」「君を私の補佐官に推薦しようと思う　君に私の背中を守ってもらいたい　わかるか　背中を任せるという事はいつでも後ろから私を撃てるという事だ」「私が道を踏み外したらその手で私を撃ち殺せ　君にはその資格がある　付いて来てくれるか」「うろたえるな！　思考を止めるな！　生きることをあきらめるな！！」「虎杖君はもう　呪術師なんですから」「雨の日は無能なんですから下がっててください大佐！」「だけどなんでかねぇ…　見捨てる気持ちにはなれねぇんだよな　そういうの!」「後は頼みます」「頼むこのまま　炭治郎のまま　死んでくれ…！！」'
+def np_softmax(x):
+    f_x = np.exp(x) / np.sum(np.exp(x))
+    return f_x
 
 @eel.expose
-def analyze_emotion(sentence_list):
-    sentence_list = split_sentences(sentence_list)
+def analyze_emotion(data_list):
+    if not data_list:
+        return json.dumps({})
+
     model_data = './src/models'
-    emotion_names_jp = ['喜び', '期待', '驚き', '悲しみ', '恐れ', '嫌悪', '怒り']  # 日本語版
+    emotion_names_jp = ['喜び', '期待', '驚き', '悲しみ', '恐れ', '嫌悪', '怒り']
 
     model = AutoModelForSequenceClassification.from_pretrained(model_data)
     tokenizer = AutoTokenizer.from_pretrained(model_data)
     model.eval()
 
-    dt = get_datetime()
     result_list = []
-    id_list = []
 
-    for id, sentence in sentence_list:
-        tokens = tokenizer(sentence, truncation=True, return_tensors="pt")
-        tokens.to(model.device)
-        preds = model(**tokens)
-        prob = np_softmax(preds.logits.cpu().detach().numpy()[0])
-        result_list.append(prob)
-        id_list.append(id)
+    for data in data_list:
+        id, text = int(data['id']), data['text']  # IDを整数型に変換
+        if not text:
+            result_list.append({'id': id, 'text': '', 'emotion': {}})
+        else:
+            tokens = tokenizer(text, truncation=True, return_tensors="pt")
+            tokens.to(model.device)
+            preds = model(**tokens)
+            prob = np_softmax(preds.logits.cpu().detach().numpy()[0])
+            emotion_result = [{emotion: float(prob) for emotion, prob in zip(emotion_names_jp, prob)}]
+            result_list.append({'id': id, 'text': text, 'emotion': emotion_result})
 
-    result_arr = np.array(result_list)
-
-    # if save_csv:
-    #     filename = "emotion_results_" + dt + ".csv"
-    #     header = ['ID'] + emotion_names_jp
-    #     with open(filename, mode='w', newline='', encoding='utf-8') as file:
-    #         writer = csv.writer(file)
-    #         writer.writerow(header)
-    #         for i, row in enumerate(result_arr):
-    #             writer.writerow([id_list[i]] + row.tolist())
-
-    #if out_dict:
-    result_dict = [{emotion: prob for emotion, prob in zip(emotion_names_jp, row)} for row in result_arr]
-    print("model_data:", result_dict)
-    return json.dumps(result_dict)  # JSONに変換して返す
+    return json.dumps(result_list)
 
 
 
@@ -134,11 +96,7 @@ def start_eel(develop):
     eel.init(directory, ['.tsx', '.ts', '.jsx', '.js', '.html'])
 
     # These will be queued until the first connection is made, but won't be repeated on a page reload
-    # say_hello_py('Python World!')
-    # eel.say_hello_js('Python World!')   # Call a JavaScript function (must be after `eel.init()`)
     get_datetime()
-
-    eel.show_log('https://github.com/samuelhwilliams/Eel/issues/363 (show_log)')
 
     eel_kwargs = dict(
         host='localhost',
@@ -158,8 +116,8 @@ def start_eel(develop):
 if __name__ == '__main__':
     import sys
 
-    print("cwd:", os.getcwd())
-    print("ospth:", os.listdir())
+    #print("cwd:", os.getcwd())
+    #print("ospth:", os.listdir())
 
     # Pass any second argument to enable debugging
     start_eel(develop=len(sys.argv) == 2)
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import theme from "./theme/theme";
-import { Button, ChakraProvider, Text } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Box } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
 import { Toaster } from "react-hot-toast";
@@ -20,52 +20,117 @@ import "ace-builds/src-noconflict/ext-language_tools"
 
 eel.set_host( 'ws://localhost:10001')
 
-// Expose the `sayHelloJS` function to Python as `say_hello_js`
-function sayHelloJS( x: string ) {
-	console.log( 'Hello from ' + x )
+// EmotionDataItem インターフェースを追加
+interface EmotionDataItem {
+	id: number;
+	text: string;
+	emotion: { [key: string]: number }[];
 }
-// WARN: must use window.eel to keep parse-able eel.expose{...}
-window.eel.expose( sayHelloJS, 'say_hello_js' )
 
-// Test anonymous function when minimized. See https://github.com/samuelhwilliams/Eel/issues/363
-function show_log(msg:string) {
-	console.log(msg)
-}
-window.eel.expose(show_log, 'show_log')
-
-// Set the default path. Would be a text input, but this is a basic example after all
-const defPath = '~'
-
-
-
-
-
-
-const App = () => {
-	const [message , setMessage] = useState(`Click button to choose a random file from the user's system`)
-	const [path , setPath] = useState(defPath)
-
-	const pickFile = () => {
-		eel.pick_file(defPath)(( message: string ) => setMessage(message) )
+const StackedBarChart: React.FC<{ emotionData: EmotionDataItem[] }> = ({ emotionData }) => {
+	// Check if emotionData is empty or not available
+	if (!emotionData || emotionData.length === 0) {
+	  // Return a placeholder or empty div if no emotion data is available
+		return <div>No emotion data available.</div>;
 	}
 
-	eel.expand_user(defPath)(( path: string ) => setPath(path) )
+	return (
+		<div>
+			{emotionData.map((dataItem, index) => (
+			<div
+				key={dataItem.id}
+				style={{
+				display: 'flex',
+				justifyContent: 'space-between',
+				marginTop: index > 0 ? '10px' : '0', // Add top margin for all but the first graph
+				}}
+			>
+				{dataItem.emotion[0] && Object.entries(dataItem.emotion[0]).map(([emotion, value]) => {
+				const cumulativeWidth = Object.values(dataItem.emotion[0])
+					.filter((v) => v > 0.1)
+					.reduce((acc, v) => acc + v, 0);
+	
+				return (
+					<div
+					key={emotion}
+					style={{
+						backgroundColor: getColor(emotion),
+						height: '18px',
+						width: `${(value / cumulativeWidth) * 100}%`,
+						border: '1px solid #fff',
+						position: 'relative',
+					}}
+					></div>
+				);
+				})}
+			</div>
+			))}
+		</div>
+	);
+};
 
-    const [editorValue, setEditorValue] = useState(""); // AceEditorのテキスト
-    const [emotionData, setEmotionData] = useState(null); // 感情分析の結果
 
-    // AceEditorのテキストが変更されたときに実行される関数
-    const handleEditorChange = (newText: string) => {
-        setEditorValue(newText); // テキストを更新
+// getColor関数にも型を指定
+const getColor = (emotion: string): string => {
+	switch (emotion) {
+		case '喜び':
+			return 'green';
+		case '期待':
+			return 'blue';
+		case '驚き':
+			return 'orange';
+		case '悲しみ':
+			return 'purple';
+		case '恐れ':
+			return 'brown';
+		case '嫌悪':
+			return 'pink';
+		case '怒り':
+			return 'red';
+		default:
+			return 'gray';
+	}
+};
 
-        // テキストが変更されたら感情分析を実行
-        eel.analyze_emotion(newText)((data: string) => {
-            const parsedData = JSON.parse(data);
-            setEmotionData(parsedData); // 感情分析の結果をセット
-			console.log("Parse:", parsedData);
-        });
+
+const App: React.FC = () => {
+
+	const [editorValue, setEditorValue] = useState(""); // AceEditorのテキスト
+	const [emotionData, setEmotionData] = useState<EmotionDataItem[]>([]); // 感情分析の結果（IDごと）
+	
+	// AceEditorのテキストが変更されたときに実行される関数
+	const handleEditorChange = (newText: string) => {
+		setEditorValue(newText);
+		
+			// Check if the text is empty
+			const trimmedText = newText.trim();
+			if (!trimmedText) {
+			// If text is empty, create empty emotion data to maintain graph spacing
+			setEmotionData([{ id: 1, text: "", emotion: [{}] }]);
+			return; // Skip emotion analysis if the text is empty
+			}
+		
+			// Perform emotion analysis only when there is non-empty text
+			const lines = trimmedText.split("\n");
+			const idTextPairs = lines.map((line, index) => ({ id: index + 1, text: line }));
+		
+			eel.analyze_emotion(idTextPairs)((data: string) => {
+			console.log("Received data:", data);
+		
+			try {
+				const parsedData = JSON.parse(data);
+				setEmotionData(parsedData);
+				console.log("Parse:", parsedData);
+			} catch (error) {
+				console.error("Error parsing JSON:", error);
+			}
+			});
+		
 		console.log("Value:", newText);
-    };
+	};
+
+
+
 
 	return (
 		<>
@@ -73,31 +138,35 @@ const App = () => {
 				<ChakraProvider theme={theme}>
 					<RecoilRoot>
 						<Toaster position="top-center" reverseOrder={false} />
-						<Text>{message}</Text>
-						<Button onClick={pickFile} >Pick Random File From `{path}`</Button>
-						<Router />
-						<AceEditor
-							placeholder="Placeholder Text"
-							height="80vh"
-							value={editorValue}
-							mode="javascript"
-							theme="monokai"
-							fontSize="16px"
-							highlightActiveLine={true}
-							setOptions={{
-								enableLiveAutocompletion: true,
-								showLineNumbers: true,
-								tabSize: 2
-							}}
-							onChange={handleEditorChange} // onChangeプロパティにテキスト変更時のハンドラを指定
-						/>
+						<Router/>
+						<Flex alignItems="flex-start">
+							<Box>
+								<AceEditor
+									placeholder="Placeholder Text"
+									height="80vh"
+									value={editorValue}
+									mode="javascript"
+									theme="monokai"
+									fontSize="16px"
+									highlightActiveLine={true}
+									setOptions={{
+										enableLiveAutocompletion: true,
+										showLineNumbers: true,
+										tabSize: 2
+									}}
+									onChange={handleEditorChange} // onChangeプロパティにテキスト変更時のハンドラを指定
+								/>
+							</Box>
+							<Box w="50%">
+								{/* Include the EmotionChart component */}
+								<StackedBarChart emotionData={emotionData} />
+							</Box>
+						</Flex>
 						<div>
-							{emotionData && (
-								<div>
-									<h2>感情分析結果:</h2>
-									<pre>{JSON.stringify(emotionData, null, 2)}</pre>
-								</div>
-							)}
+							<div>
+								<h2>感情分析結果:</h2>
+								<a>{JSON.stringify(emotionData, null, 2)}</a>
+							</div>
 						</div>
 					</RecoilRoot>
 				</ChakraProvider>
